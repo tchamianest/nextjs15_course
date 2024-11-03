855752ad4d8f3c7be492bb6e99a435b5e4cc433f
tchami <131756888+tchamianest@users.noreply.github.com>
2024-11-03 21:06:55 +0200

    Feat set up the next app for learn  next.js 15

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+   export default function Home() {
+  console.log("hello world");
+  return (
+    <div className=" h-screen flex  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-5xl font-bold">welcome to next js 15</h1>
+    </div>
+  );
+}
